0a2186e25f53fad8a955cce7a64d2207c9d343a0
Ignatz-dev <231180780+IgnacyGOAT-dev@users.noreply.github.com>
2026-05-16 22:34:55 +0200

    Komponent główny + css

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import type {SpaceObject} from "./types.ts";
+import {ObjectList} from "./ObjectList.tsx";
+import "./AstroLog.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [objects, setObjects] = useState<SpaceObject[]>([
+        {
+            id: 1,
+            name: "Mars",
+            type: "Planet",
+            distance: "225M km",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Mars_-_August_30_2021_-_Flickr_-_Kevin_M._Gill.png/1280px-Mars_-_August_30_2021_-_Flickr_-_Kevin_M._Gill.png"
+        },
+        {
+            id: 2,
+            name: "Andromeda",
+            type: "Galaxy",
+            distance: "2.5M light years",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Andromeda_Galaxy_2025.png/1920px-Andromeda_Galaxy_2025.png"
+        }
+    ]);
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    const [selected, setSelected] = useState<SpaceObject | null>(null);
 
-      <div className="ticks"></div>
+    //Masz tu funkcje by dodac do listy obiekt
+    const handleAddObject = (newObj: Omit<SpaceObject, "id">) => {
+        const completeObject: SpaceObject = {
+            ...newObj,
+            id: Date.now()
+        };
+        setObjects([...objects, completeObject]);
+    };
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+    return (
+        <div id="center">
+            <h1>🔭 AstroLog Dashboard</h1>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+            <div className="astrolog-container">
+                <ObjectList objects={objects} onSelect={setSelected} />
+
+                <div className="side-panel">
+
+                    {/* Placeholder dla wiktora*/}
+                    <div className="placeholder-card">
+                        <h3>Detale ciała niebieskiego</h3>
+                        {selected && <p style={{ color: "var(--text-h)" }}>Widzisz detale ciała: {selected.name}</p>}
+                    </div>
+
+                    {/* Placeholder dla wiktora*/}
+                    <div className="placeholder-card">
+                        <h3>Formularz dodawania ciał do listy</h3>
+                        <p>Masz funkcje handleAddObject która doda ci obiekt do listy </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
